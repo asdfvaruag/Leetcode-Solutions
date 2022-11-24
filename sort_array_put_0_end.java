eb6c9940f7e2b23c055464adbc0f0cb327ead170
Gaurav Gupta <alwar.gaurav1@gmail.com>
2022-11-24 17:43:17 +0530

    Sort Array And Put All 0 at end

---
+++ sort_array_put_0_end.java
@@ -0,0 +1,18 @@
+public class sort_array_put_0_end {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 0, 4, 3, 0, 5, 0 };
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count < n) {
+            arr[count++] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + ",");
+        }
+    }
+}
